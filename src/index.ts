43b748be7226e92afceed8877832122ff67ac1f1
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2023-04-20 21:43:17 +0530

    :tada: basic functionality

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,33 @@ const port: number = Number(process.env.PORT) || 6969;
 
 const server: FastifyInstance = fastify();
 
+interface AnimeQuery {
+	anime: string
+}
+
+interface Random {
+	QueryString: AnimeQuery
+}
+
 server.get("/", (req: FastifyRequest, res: FastifyReply) => {
 	res.send("Welcome to anime quotes! 😀");
 });
 
 server.get("/random", (req: FastifyRequest, res: FastifyReply) => {
+	// const { anime } = req.query;
+
 	res.send(animeQuotes.randomQuote());
 })
 
-server.get("/:name", (req: FastifyRequest, res: FastifyReply) => {
-	console.log(req)
+server.get("/name:name", (req: FastifyRequest<{ Params: { name: string } }>, res: FastifyReply) => {
+	// const { name } = req.params;
 	const namedQuote = animeQuotes.getQuotesByAnime(req.params.name);
-	
+	res.send(namedQuote);
+})
+
+server.get("/character:character", (req: FastifyRequest<{ Params: { character: string } }>, res: FastifyReply) => {
+	const charQuotes = animeQuotes.getQuotesByCharacter(req.params.character)
+	res.send(charQuotes);
 })
 
 const start = async () => {
